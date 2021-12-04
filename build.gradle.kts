efccbc06c53d4b00c159c3047207d1f6c9e2a475
Nozemi <nozemi95@gmail.com>
2021-12-04 22:22:20 +0100

    GitHub Actions & Gradle Changes - Added Gradle release plugin, to help with versioning. - Added a version-bumper.yml-workflow. - Added a release.yml-workflow.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.time.Year
 import java.time.MonthDay
+import java.time.Year
 
 val currentYear = Year.now().value
 val currentDay = MonthDay.now().dayOfMonth
@@ -8,14 +8,19 @@ val currentDay = MonthDay.now().dayOfMonth
 plugins {
     application
     kotlin("jvm") version "1.6.0"
+    id("net.researchgate.release") version "2.8.1"
 }
 
 group = "io.nozemi.aoc"
-version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 application {
     mainClass.set("io.nozemi.aoc.Application")
+}
+
+release {
+    failOnUpdateNeeded = false
+    revertOnFail = false
 }
 
 repositories {
@@ -26,6 +31,7 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt:3.3.0")
     implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger:1.0.3")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
@@ -37,6 +43,17 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "17"
     }
+}
+
+tasks.jar {
+    manifest {
+        attributes["Implementation-Version"] = archiveVersion
+        attributes["Main-Class"] = "io.nozemi.aoc.Application"
+    }
+
+    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
 
 tasks.test {
