710c4594374a0d7fe0468415d019e627ca61f38d
Nozemi <nozemi95@gmail.com>
2021-12-07 17:01:53 +0100

    Day 7, 2021 - Part 2 Initial code for my day 7 part 2. This code won't run at the time of this commit, because I've changed quite a bit of other stuff that will come in a later commit.

--- src/main/kotlin/io/nozemi/aoc/solutions/year2021/day07/TheTreacheryOfWhales.kt
+++ src/main/kotlin/io/nozemi/aoc/solutions/year2021/day07/TheTreacheryOfWhales.kt
@@ -1,6 +1,7 @@
 package io.nozemi.aoc.solutions.year2021.day07
 
 import io.nozemi.aoc.puzzle.Puzzle
+import kotlin.math.abs
 import kotlin.reflect.KFunction0
 
 class TheTreacheryOfWhales(input: String) : Puzzle<List<Int>>(input) {
@@ -8,29 +9,36 @@ class TheTreacheryOfWhales(input: String) : Puzzle<List<Int>>(input) {
     override fun Sequence<String>.parse(): List<Int> = this.toList()[0].split(",").map { it.trim().toInt() }
 
     override fun solutions(): List<KFunction0<Any>> = listOf(
-        ::part1
+        ::part1,
+        ::part2
     )
 
     private fun part1(): Int {
-        var target = 0
-        val maxValue = rawInput.maxOrNull() ?: 1
-        var fuelBurned = 0
+        return findFuelConsumption()
+    }
 
-        repeat(maxValue) {
-            var fuelThisIteration = 0
-            val crabs = rawInput.toMutableList()
-            while (!crabs.parallelStream().allMatch { it == target }) {
-                crabs.forEachIndexed { index, crab ->
-                    if (crab == target) return@forEachIndexed
-                    if (crab < target) crabs[index] = crab + 1
-                    if (crab > target) crabs[index] = crab - 1
-                    fuelThisIteration++
+    private fun part2(): Int {
+        return findFuelConsumption(constantBurnRate = false)
+    }
+
+    private fun findFuelConsumption(input: List<Int> = rawInput, constantBurnRate: Boolean = true): Int {
+        var targetPosition = input.minOrNull() ?: 0
+        val maxTargetPosition = input.maxOrNull() ?: 0
+
+        var cheapestOutcome = Int.MAX_VALUE
+        repeat(maxTargetPosition) {
+            val fuelConsumption = if (constantBurnRate) {
+                input.sumOf { abs(it - targetPosition) }
+            } else {
+                input.sumOf {
+                    val distance = abs(it - targetPosition)
+                    distance * (distance + 1) / 2
                 }
             }
-            if (fuelThisIteration < fuelBurned || fuelBurned == 0) fuelBurned = fuelThisIteration
-            target++
+            if (fuelConsumption < cheapestOutcome) cheapestOutcome = fuelConsumption
+            targetPosition++
         }
 
-        return fuelBurned
+        return cheapestOutcome
     }
 }
