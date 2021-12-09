4634771e480aeafb48673ab98b29b92bd20b3c4b
Nozemi <nozemi95@gmail.com>
2021-12-09 16:12:58 +0100

    Day 9, 2021 - Part 2 Initial code for part 2 of day 9. It's quite a mess, so could use some cleaning, but at least it works now!

--- src/main/kotlin/io/nozemi/aoc/solutions/year2021/day09/SmokeBasin.kt
+++ src/main/kotlin/io/nozemi/aoc/solutions/year2021/day09/SmokeBasin.kt
@@ -1,6 +1,7 @@
 package io.nozemi.aoc.solutions.year2021.day09
 
 import io.nozemi.aoc.puzzle.Puzzle
+import io.nozemi.aoc.utils.addIfNotExists
 import kotlin.reflect.KFunction0
 
 class SmokeBasin(input: String) : Puzzle<Array<IntArray>>(input) {
@@ -10,34 +11,183 @@ class SmokeBasin(input: String) : Puzzle<Array<IntArray>>(input) {
     }.toList().toTypedArray()
 
     override fun solutions(): List<KFunction0<Any>> = listOf(
-        ::part1
+        ::part1,
+        ::part2
     )
 
-    val lowPoints = mutableListOf<Int>()
+    private fun part1(): Int {
+        return getLowPoints().sumOf { it + 1 }
+    }
 
-    private fun part1(): String {
-        rawInput.forEachIndexed { row, columns ->
+    private fun part2(): Int {
+        val basins = findBasins().toMutableList()
+
+        if (basins.size <= 3) {
+            throw RuntimeException("You fucked up! You need at least 3 basins.")
+        }
+
+        val basinSizes = basins.map { it.size }.toMutableList()
+
+        val threeLargestBasins = IntArray(3) { -1 }
+        repeat(3) { iteration ->
+            val max = basinSizes.maxOrNull() ?: throw RuntimeException("You fucked up... Should never really come to this!?")
+            threeLargestBasins[iteration] = max; basinSizes.remove(max)
+        }
+
+        return threeLargestBasins[0] * threeLargestBasins[1] * threeLargestBasins[2]
+    }
+
+    private fun findBasins(): List<Basin> {
+        val basins = mutableListOf<Basin>()
+        iterateLowPoints { input, row, column, number ->
+            val positions = traverseBasin(input, mutableListOf(), number, row, column)
+            basins.add(Basin(size = positions.count(), positions = positions))
+        }
+
+        return basins
+    }
+
+    private fun aboveConditionBasin(input: Array<IntArray>, row: Int, column: Int, number: Int): Boolean =
+        (row != 0 && input[row - 1][column] < 9 && input[row - 1][column] > number)
+
+    private fun belowConditionBasin(input: Array<IntArray>, row: Int, column: Int, number: Int): Boolean =
+        (row < (input.size - 1) && input[row + 1][column] < 9 && input[row + 1][column] > number)
+
+    private fun leftConditionBasin(input: Array<IntArray>, row: Int, column: Int, number: Int): Boolean =
+        (column != 0 && input[row][column - 1] < 9 && input[row][column - 1] > number)
+
+    private fun rightConditionBasin(input: Array<IntArray>, row: Int, column: Int, number: Int): Boolean =
+        (column < (input[row].size - 1) && input[row][column + 1] < 9 && input[row][column + 1] > number)
+
+    private fun traverseBasin(
+        input: Array<IntArray>, visitedNodes: MutableList<Position>, currentNumber: Int, row: Int, column: Int
+    ): MutableList<Position> {
+        visitedNodes.addIfNotExists(Position(row, column, currentNumber))
+
+        if (aboveConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row - 1, column, input[row - 1][column]))
+        }
+        if (belowConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row + 1, column, input[row + 1][column]))
+        }
+        if (leftConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row, column - 1, input[row][column - 1]))
+        }
+        if (rightConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row, column + 1, input[row][column + 1]))
+        }
+
+        if (aboveConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row, column, input[row - 1][column]))
+            traverseBasin(input, visitedNodes, input[row - 1][column], row - 1, column)
+        } else {
+            val currentPosition = Position(row, column, currentNumber)
+            if (row == 0) {
+                //println("Nothing above $currentPosition.")
+            } else {
+                val abovePosition = Position(row - 1, column, input[row - 1][column])
+                //println("At $currentPosition we're skipping the above of $abovePosition.")
+            }
+        }
+
+        if (belowConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row, column, input[row + 1][column]))
+            traverseBasin(input, visitedNodes, input[row + 1][column], row + 1, column)
+        } else {
+            val currentPosition = Position(row, column, currentNumber)
+            if (row == input.size - 1) {
+                //println("Nothing below $currentPosition.")
+            } else {
+                val belowPosition = Position(row + 1, column, input[row + 1][column])
+                //println("At $currentPosition we're skipping the below of $belowPosition.")
+            }
+        }
+
+        if (leftConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row, column, input[row][column - 1]))
+            traverseBasin(input, visitedNodes, input[row][column - 1], row, column - 1)
+        } else {
+            val currentPosition = Position(row, column, currentNumber)
+            if (column == 0) {
+                //println("Nothing left of $currentPosition.")
+            } else {
+                val leftPosition = Position(row, column - 1, input[row][column - 1])
+                //println("At $currentPosition we're skipping the left of $leftPosition.")
+            }
+        }
+
+        if (rightConditionBasin(input, row, column, currentNumber)) {
+            visitedNodes.addIfNotExists(Position(row, column, input[row][column + 1]))
+            traverseBasin(input, visitedNodes, input[row][column + 1], row, column + 1)
+        } else {
+            val currentPosition = Position(row, column, currentNumber)
+            if (column == input[row].size - 1) {
+                //println("Nothing right of $currentPosition.")
+            } else {
+                val rightPosition = Position(row, column + 1, input[row][column + 1])
+                //println("At $currentPosition we're skipping the right of $rightPosition.")
+            }
+        }
+
+        return visitedNodes
+    }
+
+    private fun iterateLowPoints(
+        input: Array<IntArray> = rawInput,
+        doWithPosition: (input: Array<IntArray>, row: Int, column: Int, number: Int) -> Unit
+    ) {
+        input.forEachIndexed { row, columns ->
             run {
                 columns.forEachIndexed { column, number ->
                     run {
                         // Get number above if row isn't 0
-                        val above = if (row == 0) null else rawInput[row - 1][column]
-                        val below = if (row == rawInput.size - 1) null else rawInput[row + 1][column]
-                        val left = if (column == 0) null else rawInput[row][column - 1]
-                        val right = if (column == columns.size - 1) null else rawInput[row][column + 1]
-                        if (
-                            (above == null || number < above)
-                            && (below == null || number < below)
-                            && (left == null || number < left)
-                            && (right == null || number < right)
+                        val above = if (row == 0) 10 else input[row - 1][column]
+                        val below = if (row == input.size - 1) 10 else input[row + 1][column]
+                        val left = if (column == 0) 10 else input[row][column - 1]
+                        val right = if (column == columns.size - 1) 10 else input[row][column + 1]
+
+                        if ((number < above) && (number < below)
+                            && (number < left) && (number < right)
                         ) {
-                            lowPoints.add(number)
+                            doWithPosition(input, row, column, number)
                         }
                     }
                 }
             }
         }
+    }
 
-        return "$lowPoints : ${lowPoints.sumOf { it + 1 }}"
+    private fun getLowPoints(input: Array<IntArray> = rawInput): List<Int> {
+        val lowPoints = mutableListOf<Int>()
+
+        iterateLowPoints(input) { _, _, _, number ->
+            lowPoints.add(number)
+        }
+
+        return lowPoints
+    }
+
+    data class Basin(
+        val size: Int,
+        val positions: List<Position>
+    )
+
+    data class Position(
+        val row: Int,
+        val column: Int,
+        val value: Int
+    ) {
+
+        override fun equals(other: Any?): Boolean {
+            if (other == null || other !is Position) return false
+            return this.row == other.row && this.column == other.column
+        }
+
+        override fun hashCode(): Int {
+            var result = row
+            result = 31 * result + column
+            result = 31 * result + value
+            return result
+        }
     }
 }
