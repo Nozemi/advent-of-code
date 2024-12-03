b12afb8cb44c46ae52f2a33af5c609c13dea13fd
Nozemi <nozemi@dodian.net>
2024-12-03 08:55:58 +0100

    2024 - Day 3 - Part 2 Yeah, I did also fix a tiny mistake in part 1 code, but that mistake was indeed there when I submitted my answer to part 1 anyway.

--- src/main/kotlin/io/nozemi/aoc/solutions/year2024/day03/MullOverIt.kt
+++ src/main/kotlin/io/nozemi/aoc/solutions/year2024/day03/MullOverIt.kt
@@ -4,15 +4,13 @@ import io.nozemi.aoc.puzzle.Puzzle
 import kotlin.reflect.KFunction0
 
 fun main() {
-    val input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    val input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
     MullOverIt(input)
         .printAnswers()
 }
 
 class MullOverIt(input: String) : Puzzle<String>(input) {
-
-    private val patternMul = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
 
     override fun Sequence<String>.parse() = this.joinToString()
 
@@ -22,7 +20,8 @@ class MullOverIt(input: String) : Puzzle<String>(input) {
     )
 
     private fun part1(): Int {
-        val matches = patternMul.findAll(parsedInput).toList()
+        val pattern = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        val matches = pattern.findAll(parsedInput).toList()
 
         var result = 0
         matches.forEach {
@@ -30,7 +29,7 @@ class MullOverIt(input: String) : Puzzle<String>(input) {
             val right = it.groups[2]?.value?.toIntOrNull()
 
             if (left == null || right == null)
-                return 0
+                return@forEach
 
             result += left * right
         }
@@ -38,5 +37,32 @@ class MullOverIt(input: String) : Puzzle<String>(input) {
         return result
     }
 
-    private fun part2(): Int = 0
+    private fun part2(): Int {
+        val pattern = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)|(do\\(\\))|(don't\\(\\))")
+        val matches = pattern.findAll(parsedInput).toList()
+
+        var enabled = true
+        var result = 0
+        matches.forEach {
+            val match = it.groups[0]?.value
+
+            if (match == "don't()")
+                enabled = false
+            else if (match == "do()")
+                enabled = true
+
+            if (!enabled)
+                return@forEach
+
+            val left = it.groups[1]?.value?.toIntOrNull()
+            val right = it.groups[2]?.value?.toIntOrNull()
+
+            if (left == null || right == null)
+                return@forEach
+
+            result += left * right
+        }
+
+        return result
+    }
 }
