9acf40754a42ffcbd466aef3927841150eecb33c
Nozemi <aoc@nozemi.io>
2024-12-08 21:44:17 +0100

    Optimized Day 7 Solution

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt:3.3.0")
     implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger:1.0.3")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.apache.logging.log4j:log4j-core:2.15.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.15.0")
     implementation("org.junit.jupiter:junit-jupiter:5.8.2")
     implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
@@ -78,6 +78,16 @@ tasks.create<JavaExec>("runThisYear") {
     classpath = java.sourceSets["main"].runtimeClasspath
     mainClass.set("io.nozemi.aoc.Application")
     args = "-y$currentYear -d1-25 -t0".split(" ")
+}
+
+tasks.create<JavaExec>("runYesterday") {
+    group = "application"
+
+    //dependsOn(":injectNewReadme")
+
+    classpath = java.sourceSets["main"].runtimeClasspath
+    mainClass.set("io.nozemi.aoc.Application")
+    args = "-y$currentYear -d${currentDay - 1} -t0".split(" ")
 }
 
 tasks.create<JavaExec>("runToday") {
--- src/main/kotlin/io/nozemi/aoc/solutions/year2024/day07/BridgeRepair.kt
+++ src/main/kotlin/io/nozemi/aoc/solutions/year2024/day07/BridgeRepair.kt
@@ -17,54 +17,76 @@ fun main() {
             292: 11 6 16 20
         """.trimIndent()
     ).printAnswers()
+
+    //println(10 power 3)
 }
+
+infix fun Int.power(exponent: Int): Int =
+    if (exponent != 0)
+        this * (this power exponent - 1)
+    else 1
 
 class BridgeRepair(input: String) : Puzzle<Map<Long, List<Long>>>(input) {
 
-    override fun Sequence<String>.parse() = this.map {
-        val (total, numbers) = it.split(':')
+    override fun Sequence<String>.parse() =
+        this.map {
+            val (total, numbers) = it.split(':')
 
-        return@map Pair(total.toLong(), numbers.split(' ').mapNotNull(String::toLongOrNull))
-    }.toMap()
+            return@map Pair(total.toLong(), numbers.split(' ').mapNotNull(String::toLongOrNull))
+        }.toMap()
 
-    override fun solutions() = listOf(
-        ::part1,
-        ::part2
+    override fun solutions() =
+        listOf(
+            ::part1,
+            ::part2
+        )
+
+    private fun part1() =
+        parsedInput.findValidCalculations(
+            intArrayOf(1, 2)
+        )
+
+    private fun part2() =
+        parsedInput.findValidCalculations(
+            intArrayOf(1, 2, 3)
+        )
+
+    private val operators = mapOf(
+        1 to Plus(),
+        2 to Multiply(),
+        3 to Concatenate()
     )
 
-    private fun part1() = parsedInput.findValidCalculations(
-        listOf(Plus(), Multiply())
-    )
-
-    private fun part2() = parsedInput.findValidCalculations(
-        listOf(Plus(), Multiply(), Concatenate())
-    )
-
-    private fun Map<Long, List<Long>>.findValidCalculations(operators: List<Operator>) =
+    private fun Map<Long, List<Long>>.findValidCalculations(o: IntArray) =
         this.filter { (total, numbers) ->
-            if (operators.any { total == it.call(numbers) })
+            if (o.any { total == operators[it]!!.call(numbers) })
                 return@filter true
 
-            return@filter permutations(numbers.size, operators).distinct()
-                .any { combo ->
-                    total == numbers.reduceIndexed { index, a, b ->
-                        combo[index].call(listOf(a, b))
-                    }
+            return@filter permutations(numbers.size, o).firstOrNull { combo ->
+                total == numbers.reduceIndexed { index, a, b ->
+                    operators[combo[index]]?.call(listOf(a, b)) ?: 0
                 }
+            } != null
         }.map { it.key }
             .sum()
 
-    private fun permutations(size: Int, values: List<Operator>): List<Array<Operator>> {
-        if (size == 1) return values.map { arrayOf(it) }
+    private val permutationsCache = mutableMapOf<Pair<Int, IntArray>, Array<IntArray>>()
+    private fun permutations(size: Int, values: IntArray): Array<IntArray> {
+        val key = Pair(size, values)
+        if (permutationsCache.containsKey(key))
+            return permutationsCache[key]!!
 
-        val permutations = mutableListOf<Array<Operator>>()
+        if (size == 1) return values.map { intArrayOf(it) }.toTypedArray()
+
+        val permutations = mutableListOf<IntArray>()
         for (value in values) {
             val smaller = permutations(size - 1, values)
             for (permutation in smaller) {
-                permutations.add(arrayOf(value) + permutation)
+                permutations.add(intArrayOf(value) + permutation)
             }
         }
-
-        return permutations
+        
+        permutationsCache[key] = permutations.toTypedArray()
+        return permutations.toTypedArray()
     }
 }
