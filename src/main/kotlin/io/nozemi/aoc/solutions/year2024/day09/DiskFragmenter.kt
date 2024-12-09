c158509ec43211ca08bc893c3e448daf046c918f
Nozemi <aoc@nozemi.io>
2024-12-09 23:24:11 +0100

    2024, Day 9, Part 2

--- src/main/kotlin/io/nozemi/aoc/solutions/year2024/day09/DiskFragmenter.kt
+++ src/main/kotlin/io/nozemi/aoc/solutions/year2024/day09/DiskFragmenter.kt
@@ -1,6 +1,8 @@
 package io.nozemi.aoc.solutions.year2024.day09
 
 import io.nozemi.aoc.puzzle.Puzzle
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     DiskFragmenter("2333133121414131402")
@@ -12,37 +14,94 @@ class DiskFragmenter(input: String) : Puzzle<IntArray>(input) {
     override fun Sequence<String>.parse(): IntArray =
         this.joinToString("").map { it.digitToInt() }.toIntArray()
 
-    override fun solutions() =
-        listOf(
-            ::part1
-        )
+    override fun solutions() = listOf(
+        ::part1,
+        ::part2
+    )
 
     private fun part1(): Long {
-        val diskMap = mutableListOf<Int>()
+        val ffs = parsedInput.fragment()
         
+        ffs.mapIndexed { index, i -> if (i == -1) index else null }
+            .filterNotNull()
+            .forEach { freeSlot ->
+                val lastFileBlock = ffs.indexOfLast { it != -1 }
+                ffs[freeSlot] = ffs[lastFileBlock]
+                ffs[lastFileBlock] = -1
+            }
+
+        return ffs.filter { it != -1 }
+            .mapIndexed { pos, id -> 1L * id * pos }
+            .sum()
+    }
+
+    private fun part2(): Long {
+        val ffs = parsedInput.fragment()
+        
+        fun findFreeSlots(): List<IntArray> {
+            val freeGroups = mutableListOf<IntArray>()
+            val currentList = mutableListOf<Int>()
+            var previousId = Int.MIN_VALUE
+            
+            ffs.mapIndexed { index, i -> if (i == -1) index else null }
+                .filterNotNull()
+                .forEach { index ->
+                    if (max(previousId, index) - min(previousId, index) == 1 || currentList.isEmpty())
+                        currentList.add(index)
+                    else {
+                        freeGroups.add(currentList.toIntArray())
+                        currentList.clear()
+                        currentList.add(index)
+                    }
+
+                    previousId = index
+                }
+            
+            return freeGroups
+        }
+        
+        val fileIdIndexes = mutableMapOf<Int, MutableList<Int>>()
+        
+        ffs.forEachIndexed { index, i -> 
+            val list = fileIdIndexes[i] ?: mutableListOf()
+            list.add(index)
+            fileIdIndexes[i] = list
+        }
+        
+        fileIdIndexes.toSortedMap().reversed().forEach { (fileId, blocks) ->
+            val freeSlots = findFreeSlots()
+            val freeGroupIndex = freeSlots.indexOfFirst { it.size >= blocks.size && it.min() < blocks.min() }
+            if (freeGroupIndex == -1)
+                return@forEach
+            
+            val freeGroup = freeSlots[freeGroupIndex].toMutableList()
+            
+            blocks.forEach { 
+                val slot = freeGroup.removeFirst()
+                ffs[slot] = fileId
+                ffs[it] = -1
+            }
+        }
+
+        return ffs.mapIndexed { pos, id -> if (id == -1) 0 else 1L * id * pos }
+            .sum()
+    }
+    
+    private fun IntArray.fragment(): IntArray {
+        val diskMap = mutableListOf<Int>()
+
         var isFile = true
         var fileId = 0
-        parsedInput.forEach { block ->
+        this.forEach { block ->
             repeat(block) {
                 diskMap.add(if (isFile) fileId else -1)
             }
-            
+
             if (isFile) fileId++
-            
+
             isFile = !isFile
         }
-        
-        val diskMapArray = diskMap.toIntArray()
-        
-        diskMap.mapIndexedNotNull { index, i -> if (i == -1) index else null }
-            .forEach { freeSlot ->
-                val lastFileBlock = diskMapArray.indexOfLast { it != -1 }
-                diskMapArray[freeSlot] = diskMapArray[lastFileBlock]
-                diskMapArray[lastFileBlock] = -1
-            }
 
-        return diskMapArray.filter { it != -1 }
-            .mapIndexed { pos, id -> 1L * id * pos }
-            .sum()
+        return diskMap.toIntArray()
     }
 }
