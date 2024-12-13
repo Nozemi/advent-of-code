1594a226519223f48cedfaee1c4a1efd4ec21d4e
Nozemi <nozemi@dodian.net>
2024-12-13 12:11:03 +0100

    2024, Day 12, Part 2 I can't take full credit for solving this one, as I used someone else's implementation as reference.
    I was having a hard time figuring out how to count sides, and after hours of trying I had to give up and try to learn what I was doing wrong.

--- src/main/kotlin/io/nozemi/aoc/solutions/year2024/day12/GardenGroups.kt
+++ src/main/kotlin/io/nozemi/aoc/solutions/year2024/day12/GardenGroups.kt
@@ -1,9 +1,10 @@
 package io.nozemi.aoc.solutions.year2024.day12
 
-import io.nozemi.aoc.types.Coordinates
-import io.nozemi.aoc.types.matrix.CharMatrix
-import io.nozemi.aoc.types.matrix.IMatrix
-import io.nozemi.aoc.types.matrix.charMatrix
+import io.nozemi.aoc.types.Direction
+import io.nozemi.aoc.types.datastructures.distjoint.ForestDisjointSet
+import io.nozemi.aoc.types.Vector2
+import io.nozemi.aoc.types.datastructures.matrix.CharMatrix
+import io.nozemi.aoc.types.datastructures.matrix.charMatrix
 import io.nozemi.aoc.types.puzzle.Puzzle
 
 fun main() {
@@ -28,20 +29,59 @@ class GardenGroups(input: String) : Puzzle<CharMatrix>(input) {
     override fun Sequence<String>.parse() = charMatrix(this)
 
     override fun solutions() = listOf(
-        ::part1
+        ::part1,
+        ::part2
     )
 
-    private fun part1() = parsedInput.findGroups()
+    private fun part1() = parsedInput.allGroups
         .map { (crop, groups) ->
             groups.sumOf {
-                it.size * parsedInput.perimeter(crop, it).size
+                val perimeter = parsedInput.perimeter(crop, it).size
+                val area = it.size
+
+                area * perimeter
             }
         }.sum()
 
+    private fun part2() = parsedInput.allGroups
+        .map { (_, groups) ->
+            groups.sumOf {
+                val sides = parsedInput.countSides(it)
+                val area = it.size
 
-    private fun CharMatrix.perimeter(crop: Char, area: List<Coordinates>) =
+                area * sides
+            }
+        }.sum()
+
+    private fun CharMatrix.perimeter(crop: Char, area: List<Vector2>) =
         area.map { cell ->
-            this.surrounding(cell, onlyWithinBounds = false)
-                .filter { parsedInput.getAt(it) != crop }
+            this.adjacent(cell, allowOutOfBounds = true)
+                .filter { parsedInput.getAt(it.first.coordinates) != crop }
         }.flatten()
+
+    private fun CharMatrix.countSides(area: List<Vector2>): Int {
+        val sides = ForestDisjointSet<Pair<Vector2, Direction>>()
+
+        for (pos in area) {
+            val type = this.getAt(pos)
+
+            sides.addAll(
+                adjacent(pos, filter = {
+                    it.first.value != type
+                }, allowOutOfBounds = true)
+                    .map { Pair(pos, it.second) }
+            )
+        }
+
+        for (pos in area) {
+            for (direction in Direction.orthogonal) {
+                val partition1 = sides[Pair(pos, direction)] ?: continue
+                val partition2 = sides[Pair(pos + Vector2(direction.y, -direction.x), direction)] ?: continue
+
+                sides.union(partition1, partition2)
+            }
+        }
+
+        return sides.partitions
+    }
 }
